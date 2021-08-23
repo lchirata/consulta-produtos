5de658e1c3f807967b661ad6aaf212db0ad44824
lchirata <larissa.c.hirata@gmail.com>
2021-08-23 01:39:48 -0300

    add port

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 let cors = require('cors');
 let bodyParser = require('body-parser'); 
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 let api = require('./routes/api');
 
 app.get('/', (req, res) => res.send('Larissa Hirata - ST IT Cloud - Development Test LV. 3'))
 
-app.listen(process.env.PORT);
+app.listen(PORT, ()=>{ console.log( `executando na porta ${PORT}`)});
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
